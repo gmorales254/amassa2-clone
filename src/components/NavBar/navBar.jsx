0cc9a17ac39f8dfc02524ee5a9d0fefc42749934
Gaston <58697321+gmoralesintegra@users.noreply.github.com>
2022-09-28 21:54:10 -0300

    Cambio de nombre css

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,20 +1,20 @@
-import React, {useContext} from 'react'
-import "./navBar.css"
+import React, { useContext } from 'react'
+import "./navbar.css"
 import logoAmassa2 from "./amassa2Logo.png";
 import CartWidget from '../CartWidget/CartWidget';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
 
 
-const NavBar = () => {    
-    const {items} = useContext(CartContext);
+const NavBar = () => {
+    const { items } = useContext(CartContext);
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-amassa2 fixed-top">
                 <div className="container">
                     <Link to="/">
                         <span className="navbar-brand" href="/">
-                            <img src={logoAmassa2} alt="logo" className="navBarLogo"/>
+                            <img src={logoAmassa2} alt="logo" className="navBarLogo" />
                         </span>
                     </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +26,7 @@ const NavBar = () => {
                                 <a className="nav-link dropdown-toggle mt-1" href="." role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Categorias
                                 </a>
-                                <ul className="dropdown-menu"> 
+                                <ul className="dropdown-menu">
                                     <Link to="/categoria/Alfajores">
                                         <li><p className="dropdown-item enlaceCategorias">Alfajores</p></li>
                                     </Link>
@@ -38,13 +38,13 @@ const NavBar = () => {
                                     </Link>
                                 </ul>
                             </li>
-                            
+
                         </ul>
-                        {!!items.length &&<CartWidget/>}
+                        {!!items.length && <CartWidget />}
                     </div>
                 </div>
             </nav>
-            
+
         </div>
     )
 }
